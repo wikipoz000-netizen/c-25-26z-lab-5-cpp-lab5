82d8875a1178923dacf9d7e42c3dc8d8bbbec06f
wikipoz000-netizen <wikipoz000@gmail.com>
2025-11-23 16:54:25 +0100

    commit1
    Implement foo function to process a list of Humans.

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,15 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+    result.reserve(people.size());
+
+    for (Human& h : people) {
+        h.birthday();
+    }
+
+    for (auto it = people.rbegin(); it != people.rend(); ++it) {
+        result.push_back(it->isMonster() ? 'n' : 'y');
+    }
+    return result;
 }
